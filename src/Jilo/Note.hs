c91e90b6b334da5e271ef3f98a372ba2cae8a7b9
Kazark <kazark@zoho.com>
2016-02-20 14:28:27 -0500

    Pull the 0th note out of the JSON
    Took help from @mjgpy3 to get me untangled from my type errors.

--- src/Jilo/Note.hs
+++ src/Jilo/Note.hs
@@ -4,7 +4,8 @@ module Jilo.Note where
 
 import Network.Wreq
 import Control.Lens
+import Data.Aeson.Lens
 
 list baseApiUrl = do
     r <- get (baseApiUrl ++ "/notes")
-    return $ r ^. responseBody
+    return $ r ^. responseBody . key "data" . nth 0 . key "text" . _String
